151f17f840bf3b9c3fe920d05c53d09f8944f5bb
hsisco <hysensisco@gmail.com>
2020-03-19 18:36:17 -0700

    index.js + logger on server.js

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const method = req.method;
+  const url = req.originalUrl;
+  const timestamp = new Date();
 
-module.exports = server;
+  console.log(`A ${method} request to ${url} occurred at ${timestamp}.`);
+  next();
+}
+
+module.exports = {server, logger};
